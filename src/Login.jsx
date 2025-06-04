62354193235846752099fc0c5f8de9713d3dd628
yormanmp3 <yormanmp3balan2@gmail.com>
2025-06-04 19:25:25 -0400

    Zod agregado

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,10 @@
 import './global.css';
+import { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="login-container">
       <div className="left-section">
