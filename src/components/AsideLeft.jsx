b694dec5eeb364c729ce84ea9f4b2f8989626b24
yormanmp3 <yormanmp3balan2@gmail.com>
2025-06-05 12:55:32 -0400

    Change: AsideLeft.jsx se agrego nuevo chat y lista de chats

--- src/components/AsideLeft.jsx
+++ src/components/AsideLeft.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaEdit } from "react-icons/fa";
 import "../styles/AsideLeft.css";
+
+const lastChats = [
+  {
+    id: 1,
+    title: "yailin y anuel volverán?",
+    date: "2023-03-01",
+  },
+  {
+    id: 2,
+    title: "quien es mas migajero, francisco javier o haley bieber",
+    date: "2023-03-02",
+  },
+  {
+    id: 3,
+    title: "ayudame hacer mi tesis 700 paginas full referencias",
+    date: "2023-03-03",
+  },
+];
 
 export default function AsideLeft() {
   const [open, setOpen] = React.useState(false);
@@ -11,21 +30,41 @@ export default function AsideLeft() {
 
   return (
     <div className={`aside-left ${open ? "open" : ""}`}>
-      <div
-        style={{
-          cursor: "pointer",
-        }}
-        onClick={handleClickBurger}
-      >
-        <GiHamburgerMenu size={30} title="Menú lateral" />
+      <div style={{
+        marginBottom: "40px"
+      }}>
+        <GiHamburgerMenu
+          style={{
+            cursor: "pointer",
+          }}
+          onClick={handleClickBurger}
+          size={30}
+          title="Menú lateral"
+        />
       </div>
-      {/* <h2>Chats</h2> */}
-      {/* <ul className="chat-list">
-        <li>1. yailin y anuel volverán?</li>
-        <li>2. quien es mas migajero, francisco javier o haley bieber</li>
-        <li>3. ayudame hacer mi tesis 700 paginas full referencias</li>
-        <li>4. stranger things es la mejor serie</li>
-      </ul> */}
+      <div>
+        <div className="nuevo-chat">
+          <FaEdit  size={23}/>
+          Nuevo Chat
+        </div>
+        <ul className="chat-list">
+          <h2>Recientes</h2>
+          {lastChats.map((chat, index) => (
+            <li key={index}>
+              <span
+                style={{
+                  width: 500,
+                  display: "block",
+                }}
+              >
+                {chat.title.length > 30
+                  ? chat.title.substring(0, 30) + "..."
+                  : chat.title}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
